dcdbc73243ff6ec4312af105dace42d16ace62a8
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-04 23:56:45 -0400

    Modified CSS, fixed JS, and added projects to HTML

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var date = new Date().getHours()
+/* eslint-disable strict */
+var date = new Date().getHours();
 document.getElementById('greeting').innerHTML = date >= 12 && date < 17 ? 'Good Afternoon.' 
-	: date >= 17 && date < 20 ? 'Good Evening.' 
-	: (date >= 20 && date < 24) || (date >= 0 && date < 5) ? 'Hoot, Hoot, Night Owl.' 
-	: 'Good Morning.';
+  : date >= 17 && date < 20 ? 'Good Evening.' 
+    : (date >= 20 && date < 24) || (date >= 0 && date < 5) ? 'Hoot, Hoot, Night Owl.' 
+      : 'Good Morning.';
